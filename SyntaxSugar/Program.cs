804a122d8f24cd0bfd02093c26f89aad38952464
benjamin kunkel <benjamink1125@gmail.com>
2022-10-20 12:45:13 -0700

    innital commit

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to nine";
+
+Console.WriteLine(response);
+
+
+
+
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+
